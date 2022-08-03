8344c0673fb292974c1cd1f9c8e9e0c0f6e55ad8
Troy Sidle <107270605+troysidle@users.noreply.github.com>
2022-08-03 16:32:17 -0400

    More Design Elements Added
    This page uses Bootstrap.
    
    This version has a home image as a background with text.
    
    This scales well on a mobile device. It has a menu that is hidden, but can be displayed with a menu button.
    
    Google Font Montserrat has been added, bold is enabled, more format adjustments.
    
    Custom colors and other design elements added.

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// function to set a given theme/color-scheme
+        function setTheme(themeName) {
+            localStorage.setItem('theme', themeName);
+            document.documentElement.className = themeName;
+        }
+
+        // Immediately invoked function to set the theme on initial load
+        (function () 
+            {
+                setTheme('monochromeGreen');
+            }
+        )();
+
+
 /* This is edited code adopted from BootstrapMade.com. */
 
 
