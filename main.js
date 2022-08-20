b2e67e0e3554ad6f854ba614360e99fae72af05b
Troy Sidle <107270605+troysidle@users.noreply.github.com>
2022-08-20 00:02:35 -0400

    More Functionality
    More functionality and content added.

--- main.js
+++ main.js
@@ -47,15 +47,31 @@
   }
 
 
-  /* Mobile nav toggle */
-  /* Using the select and on, toggle elements as needed. */
   
-  on('click', '.mobile-nav-toggle', function(eventAction) {
-    select('body').classList.toggle('mobile-nav-active') /*toggle the class to show the navigation menu*/
-    this.classList.toggle('bi-list') /*toggle the class to show the menu (list) icon*/
-    this.classList.toggle('bi-x') /*toggle the class to show the X icon*/
+  /**
+   * Mobile navigation toggle
+   */
+  on('click', '.mobileNavigationToggleButton', function(e) {
+    select('body').classList.toggle('mobile-nav-active') /* give the body an attribute that the mobile navigation menu is showing */
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
   })
-              
+    
+    
+    
+  /**
+   * Hide Menu Button - new
+  */
+   
+  on('click', '.hideMenuArea', function(e) {
+      let body = select('body')
+      if (body.classList.contains('mobile-nav-active')) /* if the mobile navigation menu is showing */{
+        body.classList.remove('mobile-nav-active') /*stop showing the mobile navigation menu*/
+        let navbarToggle = select('.mobileNavigationToggleButton') /* the button for toggling the navigation menu */
+        navbarToggle.classList.toggle('bi-list') /* show three bars icon */
+        navbarToggle.classList.toggle('bi-x') /* show the X icon*/
+      }
+  })              
               
 /*toggle off the sliding navigation bar when user clicks outside of it*/
  /*in process*/
